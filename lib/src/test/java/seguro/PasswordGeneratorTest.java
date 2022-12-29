bd550b521b95dee4436a3e1ad5e4f0f7394139d2
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2022-12-29 23:21:46 +0100

    updates gitignore, updates generator

--- lib/src/test/java/seguro/PasswordGeneratorTest.java
+++ lib/src/test/java/seguro/PasswordGeneratorTest.java
@@ -1,9 +1,9 @@
 package seguro;
 
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class PasswordGeneratorTest {
@@ -14,13 +14,17 @@ class PasswordGeneratorTest {
     public void setup() {
         passwordGenerator = new PasswordGenerator();
     }
+
+    @Test
+    public void GeneratePasswordWithLengthZero_ShouldThrowAssertionException() {
+        assertThatThrownBy(() -> passwordGenerator.generate(0)).isInstanceOf(AssertionError.class);
+    }
     
     @Test
-    public void testPasswordGenerator() {
+    public void GeneratePassword_ShouldProduceAValidPassword() {
         String password = passwordGenerator.generate(20);
 
-
-        assert password != null;
-        assert password.length() == 20;
+        assertThat(password).isNotNull();
+        assertThat(password.length()).isEqualTo(20);
     }
 }
